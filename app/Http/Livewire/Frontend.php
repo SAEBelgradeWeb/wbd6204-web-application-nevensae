c16d28efbd0cb318cdd3ad4e3a6f0765cb9d1e55
nevensae <neven.jo7@gmail.com>
2021-04-27 20:00:08 +0200

    Loading db results in homepage

--- app/Http/Livewire/Frontend.php
+++ app/Http/Livewire/Frontend.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use App\Models\Contest;
 
 class Frontend extends Component
 {
     public function render()
     {
-        return view('livewire.frontend')->layout('layouts.frontend');
+        return view('livewire.frontend', ['data' => Contest::paginate(3)])->layout('layouts.frontend');
     }
 }
--- resources/views/livewire/frontend.blade.php
+++ resources/views/livewire/frontend.blade.php
@@ -1,3 +1,10 @@
 <div>
-    This is a frontend layout component
+    @foreach ($data as $item)
+        <img class="w-32" src="{{asset($item->image)}}" alt="Contest">
+        <h2>{{$item->title}}</h2>
+        <p>{{$item->description}}</p>
+        <a href="/contest/.{{$item->id}}">Go to contest</a>
+    @endforeach
+
+    {{$data->links()}}
 </div>
