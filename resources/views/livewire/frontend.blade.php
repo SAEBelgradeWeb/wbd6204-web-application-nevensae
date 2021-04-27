460661da6780ba09837d89fd8813b455327adebc
nevensae <neven.jo7@gmail.com>
2021-04-27 22:11:34 +0200

    Styling the home page

--- resources/views/livewire/frontend.blade.php
+++ resources/views/livewire/frontend.blade.php
@@ -1,10 +1,37 @@
 <div>
-    @foreach ($data as $item)
-        <img class="w-32" src="{{asset($item->image)}}" alt="Contest">
-        <h2>{{$item->title}}</h2>
-        <p>{{$item->description}}</p>
-        <a href="/contest/.{{$item->id}}">Go to contest</a>
-    @endforeach
+{{--    Main navigation - Extract to a component--}}
+    <div style="height: 40vh; background-color: #4a5568; opacity: 0.2;">
+{{--        Navigation--}}
+        <div>
+            <nav>
+                <ul>
+                    <li class="inline-block p-4"><a class="text-white font-serif" href="#">Home</a></li>
+                    <li class="inline-block p-4"><a class="text-white font-serif" href="#">About</a></li>
+                    <li class="inline-block p-4"><a class="text-white font-serif" href="#">Contact</a></li>
+                    <li class="inline-block p-4"><a class="text-white font-serif" href="#">Contests</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div>
+            <h2 class="text-6xl text-white text-center mt-12 font-serif">Home</h2>
+        </div>
+    </div>
 
-    {{$data->links()}}
+{{--    Main Content--}}
+    <div class="grid grid-cols-3">
+            @foreach ($data as $item)
+                <div>
+                    <img class="w-32" src="{{asset($item->image)}}" alt="Contest">
+                    <h2>{{$item->title}}</h2>
+                    <p>{{$item->description}}</p>
+                    <a href="/contest/.{{$item->id}}">Go to contest</a>
+                </div>
+            @endforeach
+    </div>
+            {{$data->links()}}
 </div>
+
+
+
+
+
