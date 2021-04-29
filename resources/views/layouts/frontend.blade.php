3af1b7c7bccfc611c516c23ef74e8e1c570ad4b0
nevensae <neven.jo7@gmail.com>
2021-04-29 14:29:05 +0200

    Added meta description and title

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="Artify the website hosting art competitions for raising awareness for environmental protection">
+    <title>{{ config('app.name', 'Artify') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
