a853bfc97b91acc4319306ca4e725e769851ad56
nevensae <neven.jo7@gmail.com>
2021-04-30 14:53:06 +0200

    Responsive 2

--- resources/views/livewire/show-contests.blade.php
+++ resources/views/livewire/show-contests.blade.php
@@ -12,20 +12,20 @@
         </div>
         <div class="mt-6" style="height: 20vh; background-image: url('https://i.picsum.photos/id/923/1920/300.jpg?hmac=RyvPEq86PCfqpRUD0bhjgIK13GLeC_apIm6qR4m2dNM'); opacity: 0.8;">
             <div>
-                <h2 class="text-6xl text-white inline-block font-serif pt-10 pl-20 uppercase">Contest - {{$title}} </h2>
+                <h2 class="lg:text-6xl md:text-5xl md:pl-10 md:pt-12 text-4xl  text-xl pt-10 pl-10  text-white inline-block font-serif lg:pt-10 lg:pl-20 uppercase">Contest - {{$title}} </h2>
             </div>
         </div>
 
         <section>
             <main>
-                <div class="flex m-20">
-                    <div class="w-3/5">
+                <div class="lg:flex flex-none lg:m-20">
+                    <div class="w-3/5 mx-auto my-10">
                         <img src="{{url(asset($image))}}" alt="contest_image">
                     </div>
-                    <div class="w-2/5 ml-20">
+                    <div class="w-2/5 lg:ml-20 mx-auto">
                         <h2 class="uppercase text-2xl font-serif mb-5">Artify</h2>
                         <h3 class="uppercase text-xl font-serif">{{$title}}</h3>
-                        <p class="font-serif ml-10">{{$description}}</p>
+                        <p class="font-serif lg:ml-10 mt-5">{{$description}}</p>
                         <h3 class="uppercase text-2xl font-serif my-5">Featured Image</h3>
                         <img class="w-3/5 ml-10" src="{{url(asset($featured_image))}}" alt="featured_image">
                         <div>
@@ -36,7 +36,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="xl:container">
+                <div class="xl:container mt-10">
                     <button class="bg-gray-800 hover:bg-gray-400 text-white font-bold py-4 px-4 rounded-l w-4/5 mb-20 ml-20" wire:click="createShowModal">
                         You may enter the competition with only one entry
                     </button>
